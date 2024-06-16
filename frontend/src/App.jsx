ae42f042c5d312a26d6cfffe6a84cb3da1e1acea
Shailesh kale <shaileshkale87730@gmail.com>
2024-06-16 16:00:43 +0530

    adding side icon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Navber from "./components/Navber/navber";
 import Hero from "./components/Hero/Hero";
@@ -9,8 +6,6 @@ import EXPERIENCE from "./components/EXPERIENCE/EXPERIENCE";
 import SKILLS from "./components/SKILLS/SKILLS";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="bg-black h-full w-full  relative ">
       <Navber />
