60d709dd6179b39bf8ea5a2e9639f729f014a24a
“shailesh <shaileshkale87730@gmail.com>
2024-06-28 18:05:48 +0530

    frontend axios adding api

--- frontend/src/components/ContactForm/ContactFrom.jsx
+++ frontend/src/components/ContactForm/ContactFrom.jsx
@@ -1,13 +1,42 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 const ContactFrom = () => {
+  const [email, setEmail] = useState("");
+  const [name, setname] = useState("");
+  const [phone, setphone] = useState(0);
+  const [companyName, setcompanyName] = useState("");
+  const [message, setmessage] = useState("");
+  const Submit = (event) => {
+    event.preventDefault();
+    try {
+      const res = axios.post(
+        "https://shailesh-portfolio-info.vercel.app/api/v1/users/create",
+        {
+          name,
+          email,
+          phone,
+          companyName,
+          message,
+        }
+      );
+
+      setEmail("");
+      setname("");
+      setphone("");
+      setcompanyName("");
+      setmessage("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div
       className=" flex flex-col flex-wrap justify-centern items-center mt-16 gap-8"
       data-aos="fade-up"
       data-aos-duration="3000"
       id="Contact">
-      <form action="" method="post" className="flex flex-col gap-4">
+      <form onSubmit={Submit} className="flex flex-col gap-4">
         <div className="flex flex-row gap-7">
           <div className="flex flex-col gap-2 ">
             <label htmlFor="name" className="text-[#22c3e6]">
@@ -18,6 +47,8 @@ const ContactFrom = () => {
               name="name"
               id="name"
               className="h-8 rounded-lg "
+              onChange={(e) => setname(e.target.value)}
+              value={name}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -29,6 +60,8 @@ const ContactFrom = () => {
               name="Email"
               id="Email"
               className="h-8 rounded-lg "
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
           </div>
         </div>
@@ -42,6 +75,8 @@ const ContactFrom = () => {
               name="Phone"
               id="Phone"
               className="h-8 rounded-lg "
+              onChange={(e) => setphone(e.target.value)}
+              value={phone}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -53,6 +88,8 @@ const ContactFrom = () => {
               name="Company_Name"
               id="Company_Name"
               className="h-8 rounded-lg "
+              onChange={(e) => setcompanyName(e.target.value)}
+              value={companyName}
             />
           </div>
         </div>
@@ -65,7 +102,9 @@ const ContactFrom = () => {
             className="w-96 rounded-lg "
             id=""
             cols="5"
-            rows="5"></textarea>
+            rows="5"
+            onChange={(e) => setmessage(e.target.value)}
+            value={message}></textarea>
         </div>
         <div className="flex flex-row justify-center items-center mt-8">
           <button
